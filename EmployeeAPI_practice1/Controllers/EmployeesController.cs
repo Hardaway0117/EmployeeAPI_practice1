052a28dc8657b5231813d61f1d1da568a37cc132
Hardaway0117 <hardaway5666@gmail.com>
2025-10-24 05:43:15 +0800

    加入專案檔案。

---
+++ EmployeeAPI_practice1/Controllers/EmployeesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI_practice1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private static readonly List<Employee> Employees = new()
+        {
+            new Employee { Id = 1, Name = "Tom", Department = "IT", Salary = 60000 },
+            new Employee { Id = 2, Name = "Amy", Department = "HR", Salary = 42000 },
+            new Employee { Id = 3, Name = "Ken", Department = "IT", Salary = 52000 },
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> GetAllEmployees()
+        {
+            return Ok(Employees);
+        }
+
+        [HttpPost]
+        public ActionResult<Employee> AddEmployee(Employee newEmployee)
+        {
+            int newId = Employees.Any() ? Employees.Max(e => e.Id) + 1 : 1;
+            newEmployee.Id = newId;
+
+            Employees.Add(newEmployee);
+
+            return CreatedAtAction(nameof(GetAllEmployees), new { id = newId }, newEmployee);
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+    }
+}
