407ec07dbce67b4b250e2ab3155215d3a087e873
Hardaway0117 <hardaway5666@gmail.com>
2025-10-25 06:18:57 +0800

    update controller - rewrite completed CRUD

--- EmployeeAPI_practice1/Controllers/EmployeesController.cs
+++ EmployeeAPI_practice1/Controllers/EmployeesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EmployeeAPI_practice1.Controllers
 {
@@ -29,6 +31,41 @@ namespace EmployeeAPI_practice1.Controllers
             Employees.Add(newEmployee);
 
             return CreatedAtAction(nameof(GetAllEmployees), new { id = newId }, newEmployee);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateEmployee(int id, Employee updateEmployee)
+        {
+            var existingEmployee = Employees.FirstOrDefault(e => e.Id == id);
+
+            if (existingEmployee == null)
+            {
+                return NotFound(new { message = $"員工 ID {id} 不存在" });
+            }
+            existingEmployee.Name = updateEmployee.Name;
+            existingEmployee.Department = updateEmployee.Department;
+            existingEmployee.Salary = updateEmployee.Salary;
+
+            return Ok
+                (new
+                {
+                    message = "員工資料更新成功",
+                    data = existingEmployee
+                }
+                );
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var existingEmployee = Employees.FirstOrDefault (e => e.Id == id);
+
+            if (existingEmployee == null)
+            {
+                return NotFound(new { message = $" ID 為 {id} 的員工不存在" });
+            }
+            Employees.Remove(existingEmployee);
+            return Ok(new { message = $"員工ID : {id}資料刪除成功" });
         }
     }
     public class Employee
